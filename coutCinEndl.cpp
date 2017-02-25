34e6a04c9c007c21e235159a772286d0c2d53d3b
atf1999 <torth212@gmail.com>
2017-02-25 10:43:32 -0500

    fixed wierd spacing

--- coutCinEndl.cpp
+++ coutCinEndl.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
 
-int main(){	
+int main(){
     //cout example
     std::cout << "Hello";
     std::cout << " I am Tom!";
 
-	//cout w/ endl
-	std::cout << "\n" << "Hello again" << std::endl;
+    //cout w/ endl
+    std::cout << "\n" << "Hello again" << std::endl;
     std::cout << "Friend" << std::endl;
-
-	//cin example
-	int x;
-	std::cout << "Input number for X: ";
-	std::cin >> x;
-	std::cout << "You inputted: " << x << "\n";
-	
-	return 0;	
+			
+    //cin example
+    int x;
+    std::cout << "Input number for X: ";
+    std::cin >> x;
+    std::cout << "You inputted: " << x << "\n";
+			
+    return 0;
 }
+			
